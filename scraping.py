bebb992d40a095e30df6db88e58bbd1eef2bf578
Programmer2011bird <maysamshaker7@gmail.com>
2024-09-12 18:22:56 +0330

    getting the prices from top 10 countries because their the cheapest

--- scraping.py
+++ scraping.py
@@ -16,24 +16,17 @@ class scraper:
         sleep(10)
         self.DRIVER.execute_script("window.scrollTo(0, (document.body.scrollHeight - 2900))")
         sleep(5)
-
+        
         self.TABLE: WebElement = self.DRIVER.find_element(By.CLASS_NAME, "table-responsive")
-
-        self.USA: WebElement = self.TABLE.find_element(By.XPATH, "//tr[contains(., 'U.S.')]")
-        self.BRAZIL: WebElement = self.TABLE.find_element(By.XPATH, "//tr[contains(., 'Brazilian')]")
-        self.UKRAINE: WebElement = self.TABLE.find_element(By.XPATH, "//tr[contains(., 'Ukrainian')]")
-        self.CHINA: WebElement = self.TABLE.find_element(By.XPATH, "//tr[contains(., 'Chinese')]")
-        self.TAIWAN: WebElement = self.TABLE.find_element(By.XPATH, "//tr[contains(., 'Taiwan')]")
-        self.PHILLIPINES: WebElement = self.TABLE.find_element(By.XPATH, "//tr[contains(., 'Philippine')]")
-        self.RUSSIA: WebElement = self.TABLE.find_element(By.XPATH, "//tr[contains(., 'Russian')]")
         
-        self.COUNTRIES: list[WebElement] = [self.USA, self.BRAZIL, self.UKRAINE, self.CHINA, self.TAIWAN, self.PHILLIPINES, self.RUSSIA]
-        
-        for _, country in enumerate(self.COUNTRIES):
-            self.CONVERTED_PRICE = country.find_element(By.CLASS_NAME, "table-prices-converted")
-            self.COUNTRY_NAME = country.find_element(By.CLASS_NAME, "price-line").text
+        for index in range(1, 11):
+            self.COUNTRY = self.TABLE.find_element(By.XPATH, f'//*[@id="prices"]/div[4]/div[1]/table/tbody/tr[{index}]')
+            
+            self.CONVERTED_PRICE = self.COUNTRY.find_element(By.CLASS_NAME, "table-prices-converted").text
+            self.COUNTRY_NAME = self.COUNTRY.find_element(By.CLASS_NAME, "price-line").text
+            
+            print(Fore.LIGHTYELLOW_EX + f"{index}. " + Fore.LIGHTGREEN_EX + self.COUNTRY_NAME + " : " + Fore.LIGHTCYAN_EX + self.CONVERTED_PRICE)
 
-            print(Fore.GREEN + self.COUNTRY_NAME + " : " + Fore.LIGHTCYAN_EX + self.CONVERTED_PRICE.text)
 
 if __name__ == "__main__":
     SCRAPER: scraper = scraper()
